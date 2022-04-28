d37138bcad2254f69fc7ed92809cb51b05ff187c
jacky2code <jacky2code@gmail.com>
2022-04-28 11:32:25 +0800

    结构体 struct

--- Contract.sol
+++ Contract.sol
@@ -1,6 +1,12 @@
-pragma solidity ^0.4.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
 contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
