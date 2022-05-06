b4747ce1826db63a4a4fd7f8f7fec18dfafbd007
jacky2code <jacky2code@gmail.com>
2022-05-06 09:40:33 +0800

    僵尸修饰符

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -9,4 +9,16 @@ contract ZombieHelper is ZombieFeeding {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+
+    // 修改僵尸名字
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    // 定制 DNA
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
 }
