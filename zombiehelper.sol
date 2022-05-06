e3ee46aaf96d949d688b86d4fb22b0ab4bc16512
jacky2code <jacky2code@gmail.com>
2022-05-06 15:30:30 +0800

    利用 view 函数,节省 Gas

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -21,4 +21,8 @@ contract ZombieHelper is ZombieFeeding {
         require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     }
+
+    function getZombiesByOwner(address _owner) external view returns (uint []) {
+        // 在这里开始
+    }
 }
