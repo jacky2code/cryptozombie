b190935d53b2d6828bb71eebaad25b454afcf0cf
jacky2code <jacky2code@gmail.com>
2022-05-06 09:12:28 +0800

    进一步了解函数修饰符

---
+++ zombiehelper.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.19;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+}
