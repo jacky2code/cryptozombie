78bd9b63564b6ff288ef27ba74542e411506569d
jacky2code <jacky2code@gmail.com>
2022-05-11 14:52:59 +0800

    僵尸战斗

--- zombieattack.sol
+++ zombieattack.sol
@@ -4,4 +4,11 @@ pragma solidity >=0.4.19;
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
+
+    uint randNonce = 0;
+
+    function randMod(uint _modulus) internal returns(uint) {
+        randNonce++;
+        return uint(keccak256(abi.encode(block.timestamp, msg.sender, randNonce))) % _modulus;
+    }
 }
